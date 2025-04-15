31e3b95d359d759e3a01d123a9ab8591809d5ca3
AlbaraaBasha <21670310075@bartin.edu.tr>
2025-04-15 13:10:33 +0300

    animated splash view

--- lib/features/splash/views/widgets/splash_view_body.dart
+++ lib/features/splash/views/widgets/splash_view_body.dart
@@ -1,8 +1,47 @@
 import 'package:bookly/core/utils/constans.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  late Animation<double> fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    );
+
+    fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: animationController, curve: Curves.easeIn),
+    );
+
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 5),
+      end: Offset.zero,
+    ).animate(
+      CurvedAnimation(parent: animationController, curve: Curves.easeOut),
+    );
+
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,9 +49,20 @@ class SplashViewBody extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image.asset(kLogo),
+        FadeTransition(opacity: fadeAnimation, child: Image.asset(kLogo)),
         const SizedBox(height: 20),
-        const Text('Free Books For Everyone', textAlign: TextAlign.center),
+        AnimatedBuilder(
+          animation: slidingAnimation,
+          builder: (context, child) {
+            return SlideTransition(
+              position: slidingAnimation,
+              child: const Text(
+                'Free Books For Everyone',
+                textAlign: TextAlign.center,
+              ),
+            );
+          },
+        ),
       ],
     );
   }
